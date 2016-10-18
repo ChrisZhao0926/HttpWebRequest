79088f18c350ec2690022db8e0e5e65c89173f61
Chris <zc850926@163.com>
2016-10-18 15:51:50 +0800

    create project with C# and class httpwebrequest
    C# HttpWebRequest

---
+++ ConsoleApplicationHttpWebRequest/ConsoleApplicationHttpWebRequest/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+
+namespace ConsoleApplicationHttpWebRequest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create("http://www.baidu.com"); //create one request
+            //HttpWebResponse response = (HttpWebResponse)request.GetResponse(); //get the response
+            //Stream responseStream = response.GetResponseStream();
+            //StreamReader streamReader = new StreamReader(responseStream, Encoding.UTF8);
+            //string html = streamReader.ReadToEnd();
+            //Console.WriteLine(html);
+            //Console.ReadKey();
+
+            HttpWebRequest request = null;
+            HttpWebResponse response = null;
+            CookieContainer cc = new CookieContainer();
+            request = (HttpWebRequest)WebRequest.Create("http://lts.sonyericsson.net/");
+            request.Method = "POST";
+            request.ContentType = "application/x-www-form-urlencoded";
+            request.UserAgent = "Mozilla/5.0 Firefox/19.0";
+
+            string requestForm = "Email=a@a.com&Password=1";
+            byte[] postdatabyte = Encoding.UTF8.GetBytes(requestForm);
+            request.ContentLength = postdatabyte.Length;
+            request.AllowAutoRedirect = true;
+            request.CookieContainer = cc;
+            request.KeepAlive = true;
+
+            Stream stream;
+            stream = request.GetRequestStream();
+            stream.Write(postdatabyte, 0, postdatabyte.Length);
+            stream.Close();
+
+            response = (HttpWebResponse)request.GetResponse();
+            Console.WriteLine();
+
+            Stream stream1 = response.GetResponseStream();
+            StreamReader sr = new StreamReader(stream1);
+            Console.WriteLine(sr.ReadToEnd());
+
+            Console.ReadKey();
+        }
+    }
+}
